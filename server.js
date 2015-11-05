08b84a1b56274a186d88ff6acc8d017db86b3e21
Iury Lazoski <lazoski@gmail.com>
2015-11-05 02:07:24 -0200

    transformando em projeto node

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('./bower_components/jquery/dist'));
+app.use(express.static('./bower_components/jquery-color'));
+app.use(express.static('./bower_components/jquery-ui'));
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: '.'});
+});
+
+app.listen(5000, () => {
+  console.log('Fago está rodando na porta 5000.');
+});
