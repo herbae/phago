b9eb60d691a355dc5abcc3ed2c647245befe51d9
Iury Lazoski <lazoski@gmail.com>
2015-11-28 00:20:40 -0200

    refactoring server loading and express endpoints

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+
+var app = module.exports = express();
+
+app.use(express.static('./bower_components/jquery/dist'));
+app.use(express.static('./bower_components/jquery-color'));
+app.use(express.static('./bower_components/jquery-ui'));
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: '.'});
+});
--- server.js
+++ server.js
@@ -5,14 +5,7 @@ var websockets = require('./websockets');
 
 var app = express();
 
-app.use(express.static('./bower_components/jquery/dist'));
-app.use(express.static('./bower_components/jquery-color'));
-app.use(express.static('./bower_components/jquery-ui'));
-app.use(express.static('./public'));
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: '.'});
-});
+app.use(require('./controllers'));
 
 var port = process.env.PORT || 5000;
 
