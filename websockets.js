858a3c7969a4396c0b74027c33d629a71fd0b02d
Iury Lazoski <lazoski@gmail.com>
2015-12-15 01:52:15 -0200

    separating ws controller in its own file

--- websockets.js
+++ websockets.js
@@ -2,9 +2,8 @@
 
 var _ = require('lodash');
 var ws = require('ws');
-var gameCtrl = require('./controllers/gameCtrl');
 var util = require('./services/util');
-var wsCtrl = wsCtrl();
+var wsCtrl = require('./controllers/wsCtrl');
 
 exports.connect = function (server) {
   var wss = new ws.Server({server: server});
@@ -20,60 +19,4 @@ exports.connect = function (server) {
       wsCtrl.shutdownGame(ws);
     });
   });
-}
-
-function wsCtrl() {
-  var clients = [];
-
-  function send(client, topic, data) {
-    client.send(JSON.stringify({topic: topic, data: data}));
-  }
-
-  function broadcast(game, topic, data) {
-    clients.filter((c) => {
-      return game.p1.id === c.id || game.p2.id === c.id;
-    }).forEach((c) => {
-      send(c, topic, data);
-    });
-  }
-
-  return {
-    welcomeNewPlayer: function(ws) {
-      ws.id = util.getRandom(10000, 90000); //TODO generate better id
-      send(ws, 'your-id', ws.id);
-      clients.push(ws);
-    },
-
-    joinGame: function(ws) {
-      ws.game = gameCtrl.joinGame(ws.id);
-      if(ws.game.p1 && ws.game.p2) {
-        broadcast(ws.game, 'new-game', ws.game);
-      }
-    },
-
-    shutdownGame: function(ws) {
-      //TODO: properly communicate clients about game ending
-      _.remove(clients, ws);
-    },
-
-    resolve: function(ws, strMessage) {
-      var message = JSON.parse(strMessage);
-      switch (message.topic) {
-        case 'move':
-          var player = ws.game.p1.id === ws.id ? 'p1' : 'p2';
-          var clientMove = message.data;
-          var move = gameCtrl.playerMove(ws.id, clientMove.move);
-          var phago = gameCtrl.newPhago(ws.id);
-
-          broadcast(ws.game, 'move', {player: player, move: move});
-          broadcast(ws.game, 'new-phago', {player: player, phago: phago});
-          broadcast(ws.game, 'remove-phago', clientMove.phagoId);
-
-          break;
-        case 'ping':
-        default:
-          break;
-      }
-    }
-  }
 }
