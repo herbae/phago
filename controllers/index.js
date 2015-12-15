00ada999603302e1c67adeee1ac96c9e3bd27878
Iury Lazoski <lazoski@gmail.com>
2015-12-15 01:41:31 -0200

    removing unnecessary dependency

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,8 @@
 'use strict';
 
 var express = require('express');
-var bodyParser = require('body-parser');
 
 var app = module.exports = express.Router();
-
-app.use(bodyParser.json());
 
 app.use(express.static('./bower_components/jquery/dist'));
 app.use(express.static('./bower_components/jquery-color'));
